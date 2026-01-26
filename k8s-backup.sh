a6efb70ee1940ce1b616421c80c57e7fa375902e
Andrey Voronkov <voronkovaa@psbank.ru>
2026-01-26 15:33:32 +0300

    Initial commit

---
+++ k8s-backup.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+# Backup all k8s resources: namespaced and clusterwide
+# Structure:
+#   <BACKUP_DIR>/
+#     clusterwide/
+#       <resourceType>/
+#         <objectName>.yaml
+#     namespaces/
+#       <namespace>/
+#         <resourceType>/
+#           <objectName>.yaml
+#
+# Usage:
+#   ./k8s-backup.sh [BACKUP_DIR]
+# If BACKUP_DIR is not set k8s-backup-<date-time> will be used
+
+set -eo pipefail
+
+if ! command -v kubectl >/dev/null 2>&1; then
+  echo "kubectl не найден в PATH" >&2
+  exit 1
+fi
+
+BACKUP_ROOT="${1:-k8s-backup-$(date +%Y%m%d-%H%M%S)}"
+
+mkdir -p "${BACKUP_ROOT}/clusterwide" "${BACKUP_ROOT}/namespaces"
+
+table_pattern="| %-70s | %-7s |\n"
+separator="------------------------------------------------------------------------------------"
+
+echo "${separator}"
+echo " Backup directory: ${BACKUP_ROOT}"
+
+echo " Collecting clusterwide resources kinds..."
+cluster_resources="$(kubectl api-resources --verbs=list --namespaced=false -o name | grep -v "/" | sort -u)"
+echo "${separator}"
+
+echo
+echo "${separator}"
+echo " Clusterwide resources backup:"
+echo "${separator}"
+echo
+printf "${table_pattern}" "Resource" "Objects"
+printf "${table_pattern}" "" "" | tr ' ' '-'
+for res in ${cluster_resources}; do
+    objs=$(kubectl get "${res}" -o name 2>/dev/null || true)
+    obj_count=0
+    res_dir="${BACKUP_ROOT}/clusterwide/${res}"
+
+    for obj in ${objs}; do
+        name="${obj#*/}"
+
+        test "${obj_count}" -eq "0" && mkdir -p "${res_dir}"
+
+        kubectl get "${obj}" -o yaml > "${res_dir}/${name}.yaml" 2>/dev/null
+        obj_count=$(( obj_count + 1 ))
+    done
+
+    printf "${table_pattern}" "${res}" "${obj_count}"
+done
+
+table_pattern="| %-25s | %-70s | %-7s |\n"
+separator="----------------------------------------------------------------------------------------------------------------"
+echo
+echo "${separator}"
+echo " Collecting namespaces list..."
+namespaces="$(kubectl get namespaces -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')"
+
+echo " Collecting namespaced resource kinds..."
+ns_resources="$(kubectl api-resources --verbs=list --namespaced=true -o name | grep -v "/" | sort -u)"
+echo "${separator}"
+
+for ns in ${namespaces}; do
+    echo
+    echo "${separator}"
+    echo " Resources backup for namespace: ${ns}"
+    echo "${separator}"
+    echo
+    ns_dir="${BACKUP_ROOT}/namespaces/${ns}"
+    mkdir -p "${ns_dir}"
+
+    printf "${table_pattern}" "Namespace" "Resource" "Objects"
+    printf "${table_pattern}" "" "" "" | tr ' ' '-'
+    for res in ${ns_resources}; do
+        objs=$(kubectl -n "${ns}" get "${res}" -o name 2>/dev/null || true)
+        obj_count=0
+        res_dir="${ns_dir}/${res}"
+
+        for obj in ${objs}; do
+            name="${obj#*/}"
+
+            test "${obj_count}" -eq "0" && mkdir -p "${res_dir}"
+
+            kubectl -n "${ns}" get "${obj}" -o yaml > "${res_dir}/${name}.yaml" 2>/dev/null
+            obj_count=$(( obj_count + 1 ))
+        done
+
+        printf "${table_pattern}" "${ns}" "${res}" "${obj_count}"
+    done
+done
+
+echo
+echo "${separator}"
+echo " Backup is saved to: ${BACKUP_ROOT}"
+echo "${separator}"
