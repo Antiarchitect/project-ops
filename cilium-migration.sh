d5c7f77a7b75ee4343bb49d60cb1440ebac67972
Andrey Voronkov <voronkovaa@psbank.ru>
2026-02-11 15:32:27 +0300

    cilium-migration.sh: add confirmation block

--- cilium-migration.sh
+++ cilium-migration.sh
@@ -7,6 +7,20 @@ NEW_NAMESPACE="k8s-cilium"
 OLD_NAMESPACE="kube-system"
 CHART_PATH="/root/helm-charts/k8s-cilium"
 
+# --- Блок защиты от случайного исполнения ---
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "ВНИМАНИЕ: Запуск миграции Cilium из ${OLD_NAMESPACE} в ${NEW_NAMESPACE}."
+echo "Убедитесь, что в ${CHART_PATH}/values.yaml установлен fullnameOverride: 'cilium'."
+echo "Это действие изменит аннотации системных ресурсов, удалит DaemonSet cilium и Deployment cilium-operator."
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo -n "Введите фразу 'Yes I am sure' для продолжения: "
+read -r CONFIRMATION
+
+if [ "${CONFIRMATION}" != "Yes I am sure" ]; then
+    echo "Ошибка: Введена неверная фраза. Выход."
+    exit 1
+fi
+
 echo "=== Шаг 1: Аннотирование Cluster-wide ресурсов (Adoption) ==="
 
 RESOURCES=(
@@ -50,13 +64,13 @@ fi
 echo "Удаляю старый оператор из ${OLD_NAMESPACE}..."
 kubectl delete -n "${OLD_NAMESPACE}" deploy cilium-operator --ignore-not-found
 
-echo "=== Шаг 3: Деплой нового чарта из ${CHART_PATH} с fullNameOverride: \"cilium\" ==="
+echo "=== Шаг 3: Деплой нового чарта из ${CHART_PATH} с fullnameOverride: \"cilium\" ==="
 /root/helm-charts/deploy.sh
 read -p "If helm-diff is ok, let's deploy new chart. Hit Enter."
 /root/helm-charts/deploy.sh deploy
 
-echo "=== Шаг 4: Деплой нового чарта из ${CHART_PATH} с fullNameOverride: \"\" ==="
-read -p "Please change fullNameOverride of ${CHART_PATH} back to \"\". Then hit Enter to deploy resources with new names."
+echo "=== Шаг 4: Деплой нового чарта из ${CHART_PATH} с fullnameOverride: \"\" ==="
+read -p "Please change fullnameOverride of ${CHART_PATH} back to \"\". Then hit Enter to deploy resources with new names."
 /root/helm-charts/deploy.sh
 read -p "If helm-diff is ok, let's deploy new chart. Hit Enter."
 /root/helm-charts/deploy.sh deploy
